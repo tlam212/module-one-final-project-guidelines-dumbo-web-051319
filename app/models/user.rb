225391708be46e035bbb13fb6de1045e44594817
Rachel R <rrrachelrath@gmail.com>
2019-05-29 11:42:35 -0400

    more methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ActiveRecord::Base
   has_many :fortunes, through: :diaries
 
   def fortune_quotes
-    self.fortunes.map {|fortune| fortune.quote}
+    self.fortunes.map {|fortune| fortune.quote}.uniq
   end
+
+  def create_new_diary
+    Diary.create(user_id: self.id, fortune_id: Fortune.all.sample.id)
+  end
+
 end
--- bin/run.rb
+++ bin/run.rb
@@ -5,6 +5,3 @@ require_relative '../config/environment'
 binding.pry
 
 puts "HELLO WORLD"
-
-cli = CommandLineInterface.new
-cli.greet
