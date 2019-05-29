d3ee95ad375582bcf2ee8c3ceafc5b128da5a6d8
Rachel R <rrrachelrath@gmail.com>
2019-05-29 11:26:27 -0400

    fixed fortunes

--- app/models/fortunes.rb
+++ app/models/fortunes.rb
@@ -1,13 +1,13 @@
 class Fortune < ActiveRecord::Base
   has_many :diaries
-  has_many :fortunes, through: :diaries
+  has_many :users, through: :diaries
 
   def self.random
     Fortune.all.sample.quote
   end
 
-  # def self.tea_leaves
-  #   "The tea leaves say... #{Fortune.all.sample.quote}"
-  # end
+  def user_names
+    self.users.map {|user| user.name}
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_many :diaries
   has_many :fortunes, through: :diaries
+
+  def fortune_quotes
+    self.fortunes.map {|fortune| fortune.quote}
+  end
 end
